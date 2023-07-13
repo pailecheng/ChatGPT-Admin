9c63205003e8b6d8a83543c509de40797d1bf6c7
pailecheng <981005580@qq.com>
2023-07-14 00:58:30 +0800

    888

--- src/views/chat/components/Message/index.vue
+++ src/views/chat/components/Message/index.vue
@@ -190,7 +190,7 @@ async function handlePreviousResponse(next: number) {
           :error="error"
           :loading="loading"
           :as-raw-text="asRawText"
-          :src="`${text.replace(window.location.origin, '')}`"
+          :src="`${text}`"
          />
         <div class="flex flex-col">
           <button
